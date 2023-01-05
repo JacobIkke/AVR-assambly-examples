d15c9c16ebee57a8d2dd1f379f6c2b1deb029c24
JacobIkke <110664522+JacobIkke@users.noreply.github.com>
2023-01-05 22:58:53 +0100

    Update pop push example.asm

--- pop push example.asm
+++ pop push example.asm
@@ -15,55 +15,54 @@ init:
 
 main:
 	
-	ldi r19, 0x05				; load 0x02 into r19
-	PUSH r19					; push it on the stack - stack 1
-	ldi r19, 0x25				; load 0x0a into r19 
-	PUSH r19					; push it on the stack - stack 2
+	ldi r19, 0x05			; load 0x02 into r19
+	PUSH r19			; push it on the stack - stack 1
+	ldi r19, 0x25			; load 0x0a into r19 
+	PUSH r19			; push it on the stack - stack 2
 
 		
-	pop r19						; r19 will load stack 2 into r19 = 0x05  last in, first out
-	sbi PORTB, PB4              ; PB4 HIGH
-    rcall delay1				; delay depenent on r19 value, higher value is longer delay
-	cbi PORTB, PB4              ; PB4 LOW
-    rcall delay1				; delay 
+	pop r19				; r19 will load stack 2 into r19 = 0x05  last in, first out
+	sbi PORTB, PB4              	; PB4 HIGH
+    	rcall delay1			; delay depenent on r19 value, higher value is longer delay
+	cbi PORTB, PB4              	; PB4 LOW
+    	rcall delay1			; delay 
 
-	rcall delay_long			; fixed delay between two examples. 
+	rcall delay_long		; fixed delay between two examples. 
 
 
-	pop r19						; r19 will load stack 1 into r19 = 0x25  last in, first out
-	sbi PORTB, PB4              ; PB4 HIGH
-    rcall delay1				; delay depenent on r19 value, higher value is longer delay
-	cbi PORTB, PB4              ; PB4 LOW
-    rcall delay1				; delay 
+	pop r19				; r19 will load stack 1 into r19 = 0x25  last in, first out
+	sbi PORTB, PB4              	; PB4 HIGH
+    	rcall delay1			; delay depenent on r19 value, higher value is longer delay
+	cbi PORTB, PB4              	; PB4 LOW
+   	 rcall delay1			; delay 
 
-	rcall delay_long			; jump to main, infinity loop
+	rcall delay_long		; jump to main, infinity loop
 
 
 	rjmp main   
 
-delay1:							; The outer loop
-    ldi r16, 255                ; Initial the timers values
-    ldi r17, 255
+delay1:					; The outer loop
+   	ldi r16, 255               	; Initial the timers values
+    	ldi r17, 255
 	mov r18, r19
-delay_i:						; The inner loop
-    dec  r16                    ; dec r16 255 times, 255 x 1 cycle delay
-    brne delay_i				; branch if not equal to beginning of timerb
-	dec  r17                    ; if r16 is not equal that dec r17 255, 255 x 1 cycle delay
-    brne delay_i				; branch if not equal to beginning of timer2 - 1 clock * 256, then 1
-    dec  r18					; if r16 is 0 that dec r17 255, 255 x 1 cycle delay
-    brne delay_i                ; Branch if not equal to beginning of timer2 - 1 clock * 5, then 1
-    ret   
+delay_i:				; The inner loop
+    	dec  r16                    	; dec r16 255 times, 255 x 1 cycle delay
+    	brne delay_i			; branch if not equal to beginning of timerb
+	dec  r17                    	; if r16 is not equal that dec r17 255, 255 x 1 cycle delay
+    	brne delay_i			; branch if not equal to beginning of timer2 - 1 clock * 256, then 1
+    	dec  r18			; if r16 is 0 that dec r17 255, 255 x 1 cycle delay
+    	brne delay_i                	; Branch if not equal to beginning of timer2 - 1 clock * 5, then 1
+    	ret   
 
-
-delay_long:						; The outer loop
-    ldi r16, 255                ; Initial the timers values
-    ldi r17, 255
+delay_long:				; The outer loop
+	ldi r16, 255                	; Initial the timers values
+	ldi r17, 255
 	ldi r18, 75
-delay_i2:						; The inner loop
-    dec  r16                    ; dec r16 255 times, 255 x 1 cycle delay
-    brne delay_i2				; branch if not equal to beginning of timerb
-	dec  r17                    ; if r16 is not equal that dec r17 255, 255 x 1 cycle delay
-    brne delay_i2				; branch if not equal to beginning of timer2 - 1 clock * 256, then 1
-    dec  r18					; if r16 is 0 that dec r17 255, 255 x 1 cycle delay
-    brne delay_i2                ; Branch if not equal to beginning of timer2 - 1 clock * 5, then 1
-    ret 
+delay_i2:				; The inner loop
+   	dec  r16                    	; dec r16 255 times, 255 x 1 cycle delay
+    	brne delay_i2			; branch if not equal to beginning of timerb
+	dec  r17                    	; if r16 is not equal that dec r17 255, 255 x 1 cycle delay
+    	brne delay_i2			; branch if not equal to beginning of timer2 - 1 clock * 256, then 1
+    	dec  r18			; if r16 is 0 that dec r17 255, 255 x 1 cycle delay
+    	brne delay_i2                	; Branch if not equal to beginning of timer2 - 1 clock * 5, then 1
+    	ret 
